small bug fix in median calculation

src/functions.py
@@ -96,7 +96,7 @@ def weighted_mean(df, weight_column, numeric_column):
     cum_weights = sorted_df[weight_column].cumsum()
     median_index = np.searchsorted(cum_weights, total_weight / 2.0)
     
-    if cum_weights.iloc[median_index] == total_weight / 2.0 or sorted_df.shape[0] == 1:
+    if cum_weights.iloc[median_index] == total_weight / 2.0 or sorted_df.shape[0] <= 2:
         weighted_median_result = sorted_df.iloc[median_index][numeric_column]
     else:
         weighted_median_result = sorted_df.iloc[median_index + 1][numeric_column]